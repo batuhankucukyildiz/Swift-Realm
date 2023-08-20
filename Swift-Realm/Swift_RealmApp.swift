432086154f78b157aac7a52568e81b33a2465e9c
Batuhan <kucukyildizbatuhan@gmail.com>
2023-08-20 23:14:57 +0300

    Initial Commit

---
+++ Swift-Realm/Swift_RealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_RealmApp.swift
+//  Swift-Realm
+//
+//  Created by Batuhan Küçükyıldız on 20.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_RealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
